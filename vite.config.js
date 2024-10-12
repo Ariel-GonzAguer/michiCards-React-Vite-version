94325cd6aace22e72713ce26fdbe0d4c7c1a29e6
Ariel-GonzAguer <ariegonzaguer@gmail.com>
2024-10-12 14:35:35 -0600

    PWA agregado

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "/icons/shieldCatSolidFontAwsome.svg"
+      ],
+      manifest: {
+        name: "Michi Cards",
+        short_name: "MichiCards",
+        description: "Create Your Own MichiCards Collection!",
+        theme_color: "pink",
+        icons: [
+          {
+            src: "/icons/shieldCatSolidFontAwsome.svg",
+            sizes: "192x192",
+            type: "image/svg+xml",
+          },
+          {
+            src: "/icons/shieldCatSolidFontAwsome.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+          },
+          {
+            src: "/icons/shieldCatSolidFontAwsome.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
     globals: true,
-    setupFiles: './setupTests.js',
-  }
-})
+    setupFiles: "./setupTests.js",
+  },
+});
